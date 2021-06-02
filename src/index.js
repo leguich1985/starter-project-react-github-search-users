1c5e4a9da92f387db511ae514b24f41bd86e2437
leguich1985 <leguich@mail.ru>
2021-06-02 16:21:18 +0300

    add auth

--- src/index.js
+++ src/index.js
@@ -8,9 +8,15 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain="dev-82f2tdif.eu.auth0.com"
+      clientId="7fldKZBFXstQOgYRdQK4NxTwHesPrFko"
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
