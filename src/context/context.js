782afcfbe8a5a584da365726525a416ecdabcfae
leguich1985 <leguich@mail.ru>
2021-06-02 15:15:56 +0300

    add allSettled

--- src/context/context.js
+++ src/context/context.js
@@ -25,6 +25,23 @@ const GithubProvider = ({ children }) => {
     console.log("=~> ", "response", response);
     if (response) {
       setGithubUser(response.data);
+      const { login, followers_url } = response.data;
+
+      await Promise.allSettled([
+        axios(`${rootUrl}/users/${login}/repos?per_page=100`),
+        axios(`${followers_url}?per_page=100`),
+      ])
+        .then((results) => {
+          const [repos, followers] = results;
+          const status = "fulfilled";
+          if (repos.status === status) {
+            setRepos(repos.value.data);
+          }
+          if (followers.status === status) {
+            setFollowers(followers.value.data);
+          }
+        })
+        .catch((err) => console.log("=~> ", "err", err));
     } else {
       toggleError(true, "no user found");
     }
