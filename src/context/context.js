3614fa6625ceea9f440ca47577612ea6910ca222
leguich1985 <leguich@mail.ru>
2021-06-02 14:21:10 +0300

    add errors handle

--- src/context/context.js
+++ src/context/context.js
@@ -12,8 +12,54 @@ const GithubProvider = ({ children }) => {
   const [githubUser, setGithubUser] = useState(mockUser);
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
+  const [requests, setRequests] = useState(0);
+  const [loading, setIsLoading] = useState(false);
+  const [error, setError] = useState({ show: false, msg: "" });
+
+  const searchGithubUser = async (user) => {
+    toggleError();
+    const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
+      console.log("=~> ", "err", err)
+    );
+    console.log("=~> ", "response", response);
+    if (response) {
+      setGithubUser(response.data);
+    } else {
+      toggleError(true, "no user found");
+    }
+  };
+
+  const checkRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data;
+        setRequests(remaining);
+        if (remaining === 0) {
+          toggleError(true, "Sorry, rate limit");
+        }
+      })
+      .catch((err) => console.log("=~> ", "err", err));
+  };
+
+  function toggleError(show = false, msg = "") {
+    setError({ show, msg });
+  }
+
+  useEffect(() => checkRequests(), []);
+
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+    <GithubContext.Provider
+      value={{
+        githubUser,
+        repos,
+        followers,
+        requests,
+        error,
+        searchGithubUser,
+      }}
+    >
       {children}
     </GithubContext.Provider>
   );
